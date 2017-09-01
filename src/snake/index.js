7ed420f52b11e18aa6215499f2cade24114e1289
daialihexiaoniu <397698632@qq.com>
2017-09-01 14:47:09 +0800

    修改方法，但是还是有问题

--- src/snake/index.js
+++ src/snake/index.js
@@ -7,8 +7,10 @@ $(function () {
     var x = 0, y = k-1;
     var ji = null;
     var mark = 0;
+    var m,n;
 
     initMap();
+    food();
 
     $('body').keydown(function (e) {
         if(e.keyCode === 37) { //right
@@ -31,7 +33,7 @@ $(function () {
 
     function initMap() {
         for(var i = 0; i < ck; i++) {
-            var tr = document.getElementById("container").insertRow(-1);;
+            var tr = document.createElement("tr");
             var trs = [];
             for(var j = 0; j < ck; j++) {
                 var td = document.createElement('td');
@@ -46,6 +48,8 @@ $(function () {
             she[g] = allTds[0][g];
             allTds[0][g].className = "hei";
         }
+
+        console.log(she)
     }
 
     function hide(el) {
@@ -57,47 +61,104 @@ $(function () {
     }
 
     function start() {
-        console.log('start')
-        hide(she);
-        for(var o = 0; o < she.length; o++) {
-            she[o] = she[o + 1]
-        }
         if (dir === 1) {
-            if (x - 1 >= 0) {
-                she[k-1] = allTds[x-1][y];
-                x-=1;
+            if ((x - 1) >= 0) {
+                if(eatSelf(allTds[x-1][y])) {
+                    hide(she)
+                    if((x - 1) === m & y === n) {
+                       eatFood()
+                   } else {
+                       for(var o=0;o<she.length-1;o++) {
+                           she[o] = she[o+1];
+                       }
+                       she[k-1] = allTds[x-1][y];
+                   }
+                   show(she)
+                    x -= 1;
+                    ji = setTimeout(function() {
+                        start()
+                    }, 200);
+                } else {
+                    alert('eat top')
+                }
             } else {
                 alert('top')
             }
         }
         if (dir === 2) {
-            if (y + 1 < ck) {
-                she[k-1] = allTds[x][y+1];
-                y+=1;
+            if ((y + 1) < ck) {
+                if(eatSelf(allTds[x][y+1])) {
+                    hide(she)
+                    if(x === m && (y + 1) === n) {
+                        eatFood()
+                    } else {
+                        for(var o=0;o<she.length-1;o++) {
+                            she[o] = she[o+1];
+                        }
+                        she[k-1] = allTds[x][y+1];
+                    }
+                    show(she)
+                    y += 1;
+                    ji = setTimeout(function() {
+                        start()
+                    }, 200);
+                } else {
+                    alert('eat right')
+                }
             } else {
                 alert('right ')
             }
         }
         if (dir === 3) {
-            if (x + 1 < ck) {
-                she[k-1] = allTds[x+1][y];
-                x+=1;
+            if ((x + 1) < ck) {
+                if(eatSelf(allTds[x+1][y])) {
+                    hide(she)
+                    if((x + 1) === m && y === n) {
+                        eatFood()
+                    } else {
+                        for(var o=0;o<she.length-1;o++) {
+                            she[o] = she[o+1];
+                        }
+                        she[k-1] = allTds[x+1][y];
+                    }
+                    show(she)
+                    x+=1;
+                    ji = setTimeout(function() {
+                        start()
+                    }, 200);
+                } else {
+                    alert('eat bottom')
+                }
             } else {
                 alert('bottom')
             }
         }
         if (dir === 4) {
-            if (y - 1 >= 0) {
-                she[k - 1] = allTds[x][y-1];
-                y -=1;
+            if ((y - 1) >= 0) {
+                if(eatSelf(allTds[x][y-1])) {
+                    hide(she)
+                    if(x === m && (y-1) === n) {
+                        eatFood()
+                    } else {
+                        for(var o=0;o<she.length-1;o++) {
+                            she[o] = she[o+1];
+                        }
+                        she[k - 1] = allTds[x][y-1];
+                    }
+                    show(she)
+                    y -= 1;
+                    ji = setTimeout(function() {
+                        start()
+                    }, 200);
+                } else {
+                    alert('eat left')
+                }
+
             } else {
                 alert('left')
             }
         }
-        show(she);
-        setTimeout(function() {
-            start()
-        }, 200);
+
     }
 
     function stop() {
@@ -106,6 +167,36 @@ $(function () {
         }
     }
 
+    function eatSelf(ch) {
+        var flag = true;
+        for(var i = 0; i < she.length; i++) {
+            if(she[i] == ch) {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    function eatFood() {
+        console.log('eat food')
+        she[she.length] = allTds[m][n];
+        k += 1;
+        mark += 1;
+        food();
+        console.log(k)
+    }
+
+    function food() {
+        console.log('food')
+        m = Math.floor(Math.random()*ck);
+        n = Math.floor(Math.random()*ck);
+        if(allTds[m][n].className === 'hei') {
+            console.log('in if')
+            food()
+        } else {
+            allTds[m][m].className = 'red';
+        }
+    }
 });
 
 
