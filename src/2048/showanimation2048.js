c330fba31074dbc64941ca602e950912a6ae2967
daialihexiaoniu <397698632@qq.com>
2017-09-04 10:12:26 +0800

    2048小游戏

---
+++ src/2048/showanimation2048.js
@@ -0,0 +1,28 @@
+
+function showNumberWithAnimation( i , j , randNumber ){
+
+    var numberCeil = $('#number-ceil-' + i + "-" + j );
+
+    numberCeil.css('background-color',getNumberBackgroundColor( randNumber ) );
+    numberCeil.css('color',getNumberColor( randNumber ) );
+    numberCeil.text( randNumber );
+
+    numberCeil.animate({
+        width:"100px",
+        height:"100px",
+        top:getPosTop( i , j ),
+        left:getPosLeft( i , j )
+    },50);
+}
+
+function showMoveAnimation(fromx,fromy,tox,toy){
+    var numberCeil=$('#number-ceil-'+fromx+'-'+fromy);
+    numberCeil.animate({
+        top:getPosTop(tox,toy),
+        left:getPosLeft(tox,toy)
+    },200);
+}
+
+function updateScore(score){
+    $('#score').text(score);
+}
